88283ebe60daf8690382bbfaac92069d61b2c072
Yiheng Shu <lzw429@qq.com>
2017-07-10 09:12:37 +0800

    Add file via upload

--- 一元二次方程.py
+++ 一元二次方程.py
@@ -1,10 +1,17 @@
 import math
-def quadratic(a,b,c):
-    if b*b-4*a*c>=0:
-        return (-b+math.sqrt(b*b-4*a*c))/(2*a),(-b-math.sqrt(b*b-4*a*c))/(2*a)
+
+
+def quadratic(a, b, c):
+    if b * b - 4 * a * c >= 0:
+        return (-b + math.sqrt(b * b - 4 * a * c)) / (2 * a), (-b - math.sqrt(b * b - 4 * a * c)) / (2 * a)
     else:
-        return 
-a=input("a=")
-b=input("b=")
-c=input("c=")
-print(quadratic(a,b,c))
+        res1 = str(-b / (2 * a)) + '+' + str(math.sqrt(abs(b * b - 4 * a * c))) + 'i'
+        res2 = str(-b / (2 * a)) + '-' + str(math.sqrt(abs(b * b - 4 * a * c))) + 'i'
+        return res1 + res2
+
+
+print("Please input a, b and c")
+a = input()
+b = input()
+c = input()
+print(quadratic(a, b, c))
